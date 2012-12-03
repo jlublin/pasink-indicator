e21bf76bdbb863eda487182cc3f533c88382f36b
Joachim Lublin <jlubin@gmail.com>
2012-12-03 23:56:24 +0100

    Added use of pactl for items

--- sinkchooser-indicator.py
+++ sinkchooser-indicator.py
@@ -1,6 +1,13 @@
+# For now this will use pactl until I have figured out how to use libpulseaudio
+
+# >>> from subprocess import check_output
+# >>> a = check_output(['ls', '-l'])
 
 from gi.repository import Gtk, GLib
 from gi.repository import AppIndicator3
+
+from subprocess import check_output
+import re
 
 class Indicator:
 	def __init__(self):
@@ -8,18 +15,29 @@ class Indicator:
 
 		self.indicator.set_status(AppIndicator3.IndicatorStatus.ACTIVE)
 
-		self.menu = Gtk.Menu()
+		self.UpdateSinks()
 
-		item = Gtk.MenuItem()
-		item.set_label('Exit')
-		item.connect('activate', self.exit)
-		item.show()
-		self.menu.append(item)
+		self.indicator.set_label('PulseSinker', '')
+
+	def UpdateSinks(self):
+		self.GetSinks()
+
+		self.menu = Gtk.Menu()
+		
+		for sink in self.sinks:
+			item = Gtk.MenuItem()
+			item.set_label(sink)
+			item.connect('activate', self.exit)
+			item.show()
+			self.menu.append(item)
 
 		self.menu.show()
 		self.indicator.set_menu(self.menu)
+		
 
-		self.indicator.set_label('PulseSinker', '')
+	def GetSinks(self):
+		pactl_output = check_output(['pactl', 'list'])
+		self.sinks = re.findall('Sink #.*', pactl_output)
 
 	def exit(self, event):
 		Gtk.main_quit()
